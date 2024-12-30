07ca77666a21422794994e1345d7d27eb45b2f03
Abdurrahman <arahmanmh15@gmail.com>
2024-12-30 14:02:29 +0800

    08 highest rated and popular book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -19,4 +19,17 @@ class Book extends Model
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+
+    }
+
+    //"select `books`.*, (select count(*) from `reviews` where `books`.`id` = `reviews`.`book_id`) as `reviews_count`, (select avg(`reviews`.`rating`) from `reviews` where `books`.`id` = `reviews`.`book_id`) as `reviews_avg_rating` from `books` order by `reviews_count` desc, `reviews_avg_rating` desc"
 }
