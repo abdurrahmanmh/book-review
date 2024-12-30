ee77a963abce8a056ee254a54260b310c6c61ac8
Abdurrahman <arahmanmh15@gmail.com>
2024-12-30 11:35:04 +0800

    03 one to many

--- database/migrations/2024_12_30_025138_create_reviews_table.php
+++ database/migrations/2024_12_30_025138_create_reviews_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,10 +13,19 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
 
+            // $table->unsignedBigInteger('book_id');
+
             $table->text('review');
             $table->unsignedTinyInteger('rating');
 
             $table->timestamps();
+
+            //on cascade delete everything
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+
+            // shorter syntax
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete();
+
         });
     }
 
